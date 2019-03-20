b9f2d8ed39dde7579be28addd13a4eb66b63b1bb
mjovani <mjovani@helius-software.com>
2019-03-20 21:05:58 +0100

    DB config update

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'forge'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
